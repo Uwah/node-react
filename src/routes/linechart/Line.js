0e9a5f5e71fb8adbc5cc10d655e4820864f88648
leung0601@163.com <leung0601@163.com>
2018-08-19 23:06:27 +0800

    增加tooltip

--- src/routes/linechart/Line.js
+++ src/routes/linechart/Line.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 const echarts = require('echarts/lib/echarts');
 require('echarts/lib/chart/bar');
 // // 引入提示框和标题组件
-// require('echarts/lib/component/tooltip');
-// require('echarts/lib/component/title');
+require('echarts/lib/component/tooltip');
+require('echarts/lib/component/title');
 
 
 
